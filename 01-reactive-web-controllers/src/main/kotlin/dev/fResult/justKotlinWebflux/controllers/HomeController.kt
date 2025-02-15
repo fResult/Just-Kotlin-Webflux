bfe6c79eb58d2323ab4da833ff72a220685284ba
fResult <styxmaz@gmail.com>
2025-02-15 18:16:11 +0700

    Create `Add New Employee` form in Home page.

--- 01-reactive-web-controllers/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/HomeController.kt
+++ 01-reactive-web-controllers/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/HomeController.kt
@@ -3,6 +3,8 @@ package dev.fResult.justKotlinWebflux.controllers
 import dev.fResult.justKotlinWebflux.entities.Employee
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.ModelAttribute
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.reactive.result.view.Rendering
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -18,9 +20,17 @@ class HomeController(
   private val idGenerator: AtomicLong = AtomicLong(database.size.toLong())
 ) {
   @GetMapping
-  fun index(): Mono<Rendering> {
-    return Flux.fromIterable(database.values).collectList().map {
-      Rendering.view("index").modelAttribute("employees", it).build()
-    }
+  fun index(): Mono<Rendering> = Flux.fromIterable(database.values).collectList().map {
+    Rendering.view("index")
+      .modelAttribute("employees", it)
+      .modelAttribute("newEmployee", Employee("", ""))
+      .build()
   }
+
+  @PostMapping("/new-employee")
+  fun newEmployee(@ModelAttribute newEmployee: Mono<Employee>): Mono<String> = newEmployee
+    .doOnNext { employee ->
+      val id = idGenerator.incrementAndGet()
+      database[id] = Employee(id, employee.name, employee.role)
+    }.thenReturn("redirect:/")
 }
