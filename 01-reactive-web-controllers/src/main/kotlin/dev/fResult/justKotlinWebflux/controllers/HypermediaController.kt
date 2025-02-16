1e85aaf1c10dd85e7116ca09576984b40b3de251
fResult <styxmaz@gmail.com>
2025-02-16 16:30:16 +0700

    Refactor by extracting method to `toEmployeeEntityModelMono`.

--- 01-reactive-web-controllers/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/HypermediaController.kt
+++ 01-reactive-web-controllers/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/HypermediaController.kt
@@ -3,15 +3,13 @@ package dev.fResult.justKotlinWebflux.controllers
 import dev.fResult.justKotlinWebflux.entities.Employee
 import org.springframework.hateoas.CollectionModel
 import org.springframework.hateoas.EntityModel
+import org.springframework.hateoas.Link
 import org.springframework.hateoas.config.EnableHypermediaSupport
 import org.springframework.hateoas.config.EnableHypermediaSupport.HypermediaType
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
@@ -36,25 +34,30 @@ class HypermediaController(
   private val idGenerator: AtomicLong = AtomicLong(database.size.toLong())
 ) {
   @GetMapping
-  fun employees(): Mono<CollectionModel<EntityModel<Employee>>> {
-    val selfLinkMono = linkTo(methodOn(this::class.java).employees()).withSelfRel().toMono()
+  fun allEmployees(): Mono<CollectionModel<EntityModel<Employee>>> {
+    val selfLinkMono = linkTo(methodOn(this::class.java).allEmployees()).withSelfRel().toMono()
 
     return selfLinkMono.flatMap { selfLink ->
-      Flux.fromIterable(database.keys).flatMap(::employee).collectList()
+      Flux.fromIterable(database.keys).flatMap(::employeeById).collectList()
         .map { entityModels -> CollectionModel.of(entityModels, selfLink) }
     }
   }
 
   @GetMapping("/{id}")
-  fun employee(@PathVariable id: Long): Mono<EntityModel<Employee>> {
-    val selfLinkMono = linkTo(methodOn(this::class.java).employee(id)).withSelfRel().toMono()
-    val aggregateRootLinkMono = linkTo(methodOn(this::class.java).employees()).withRel("employees").toMono()
+  fun employeeById(@PathVariable id: Long): Mono<EntityModel<Employee>> {
+    val selfLinkMono = linkTo(methodOn(this::class.java).employeeById(id)).withSelfRel().toMono()
+    val aggregateRootLinkMono = linkTo(methodOn(this::class.java).allEmployees()).withRel("employees").toMono()
     val linksMono = Mono.zip(selfLinkMono, aggregateRootLinkMono) { self, aggregateRoot -> self to aggregateRoot }
 
-    return linksMono.flatMap { linksPair ->
-      database[id]?.let { employee ->
-        Mono.just(EntityModel.of(employee, linksPair.first, linksPair.second))
-      } ?: Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND, "Employee with ID $id not found"))
+    return linksMono.flatMap(toEmployeeEntityModelMono(id))
+  }
+
+  private fun toEmployeeEntityModelMono(id: Long): (Pair<Link, Link>) -> Mono<EntityModel<Employee>> = { linksPair ->
+    val toEntityModel: (Employee) -> Mono<EntityModel<Employee>> = { employee ->
+      Mono.just(EntityModel.of(employee, linksPair.first, linksPair.second))
     }
+
+    database[id]?.let(toEntityModel)
+      ?: Mono.error(ResponseStatusException(HttpStatus.NOT_FOUND, "Employee with ID $id not found"))
   }
 }
