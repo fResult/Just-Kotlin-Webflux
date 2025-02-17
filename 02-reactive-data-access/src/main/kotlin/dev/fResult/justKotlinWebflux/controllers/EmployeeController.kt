4aa34949d9ca1d5d9b6b190fd111e66c37922197
fResult <styxmaz@gmail.com>
2025-02-18 01:52:21 +0700

    Create GET `/api/employees` API endpoint.

--- 02-reactive-data-access/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/EmployeeController.kt
+++ 02-reactive-data-access/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/EmployeeController.kt
@@ -1,8 +1,17 @@
 package dev.fResult.justKotlinWebflux.controllers
 
+import dev.fResult.justKotlinWebflux.entities.Employee
+import dev.fResult.justKotlinWebflux.repositories.EmployeeRepository
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
 
 @RestController
 @RequestMapping("/api/employees")
-class EmployeeController
+class EmployeeController(private val employeeRepository: EmployeeRepository) {
+  @GetMapping
+  fun all(): Mono<ResponseEntity<List<Employee>>> = employeeRepository.findAll()
+    .collectList().map { ResponseEntity.ok(it) }
+}
