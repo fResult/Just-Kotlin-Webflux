440a6d7cad2fcc17b61b4bdd550ad2eee1f78ddc
fResult <styxmaz@gmail.com>
2025-02-18 01:52:42 +0700

    Create GET `/api/employees/:id` API endpoint.

--- 02-reactive-data-access/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/EmployeeController.kt
+++ 02-reactive-data-access/src/main/kotlin/dev/fResult/justKotlinWebflux/controllers/EmployeeController.kt
@@ -4,9 +4,11 @@ import dev.fResult.justKotlinWebflux.entities.Employee
 import dev.fResult.justKotlinWebflux.repositories.EmployeeRepository
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.switchIfEmpty
 
 @RestController
 @RequestMapping("/api/employees")
@@ -14,4 +16,11 @@ class EmployeeController(private val employeeRepository: EmployeeRepository) {
   @GetMapping
   fun all(): Mono<ResponseEntity<List<Employee>>> = employeeRepository.findAll()
     .collectList().map { ResponseEntity.ok(it) }
+
+  @GetMapping("/{id}")
+  fun byId(@PathVariable id: Long): Mono<ResponseEntity<Employee>> = employeeRepository.findById(id)
+    .map {
+      ResponseEntity.ok(it)
+    }
+    .switchIfEmpty { Mono.just(ResponseEntity.notFound().build()) }
 }
